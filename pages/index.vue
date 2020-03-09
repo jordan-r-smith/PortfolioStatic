618dcffbd978b07e6e9ab6f75c055195f4eea4e9
Jordan Smith <jordan.r.smith@gmail.com>
2020-03-08 23:31:28 -0400

    Create hero section. Cleanup default files. Add iconset.

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,87 @@
 <template>
-  <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        jordan-portfolio
-      </h1>
-      <h2 class="subtitle">
-        My personal portfolio.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <section class="hero">
+            <div class="hero-body container">
+                <h1 class="title is-1">
+                    <span class="title-accent">Jordan</span> Smith
+                </h1>
+
+                <!-- About -->
+                <p>
+                    I am a web developer located in Roswell, Georgia and am interested in developing functional and elegant code. I have
+                    experience developing applications in Angular, DotNet, Rails, TypeScript, HTML, CSS and Java. You can contact me
+                    using any of the links below.
+                </p>
+                <!-- /About -->
+
+                <!-- Social -->
+                <div class="columns social-media">
+                    <div class="column">
+                        <a href="https://github.com/jordan-r-smith">
+                            <fa :icon="['fab', 'github']" class="fa-4x" />
+                        </a>
+                    </div>
+                    <div class="column">
+                        <a href="https://www.linkedin.com/in/jordanrsmith">
+                            <fa :icon="['fab', 'linkedin']" class="fa-4x" />
+                        </a>
+                    </div>
+                    <div class="column">
+                        <a href="mailto:jordan.r.smith@gmail.com">
+                            <fa :icon="['fas', 'envelope']" class="fa-4x" />
+                        </a>
+                    </div>
+                </div>
+                <!-- /Social -->
+            </div>
+        </section>
+
+        <section class="section container">
+            <showcase />
+        </section>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import showcase from "~/components/showcase";
 
 export default {
-  components: {
-    Logo
-  }
-}
+    components: {
+        showcase
+    }
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.hero {
+    background-color: #7091b9;
+    color: #fff;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    color: #fff;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.hero-body p {
+    font-size: 20px;
+    font-weight: 400;
 }
 
-.links {
-  padding-top: 15px;
+.title-accent {
+    color: #283c54;
+}
+
+.social-media {
+    margin: 10px 0 5px 0;
+    text-align: center;
+}
+
+.social-media a {
+    color: #fff;
+    transition: color 300ms ease-out;
+}
+
+.social-media a:hover {
+    color: #283c54;
 }
 </style>
